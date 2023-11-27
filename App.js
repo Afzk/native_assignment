9d390bfa0ff4d7fed495db34cf1a17770934a35c
Afzal <afzalkhanahmed@gmail.com>
2023-11-27 12:28:41 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+import StackNavigator from './navigation/StackNavigator';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <StackNavigator />
+    </Provider>
+  );
+};
+
+export default App;
